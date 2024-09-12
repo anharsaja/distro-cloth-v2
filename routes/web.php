1a8b95b695e95ab115c82c75e332e579c396c8bb
anharsaja <anharmuk12@gmail.com>
2024-09-13 00:40:10 +0700

    feat: success update lastest assets

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home.index');
 });
+
+Route::get('/login', function () {})->name('login');
+Route::get('/register', function () {})->name('register');
+Route::get('/shop.wishlist', function () {})->name('shop.wishlist');
+Route::get('/shop.cart', function () {})->name('shop.cart');
+Route::get('/shop', function () {})->name('shop');
